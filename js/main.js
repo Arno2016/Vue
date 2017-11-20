c48881048f2cad355e98de492258be57977be223
harvey <870176709@qq.com>
2017-11-20 23:46:13 +0800

    2017-11-20

--- js/main.js
+++ js/main.js
@@ -4,3 +4,56 @@ new Vue({
 		message:'hello world !'
 	}
 })
+
+var app2=new Vue({
+	el:'#app2',
+	data:{
+		message:'hello app2 ! And now is '+new Date().toLocaleString()
+	}
+})
+
+var app3=new Vue({
+	el:'#app3',
+	data:{
+		message:'Now,you can see me!',
+		flag:true,
+		users:[
+			{name:'harvey1',age:12},
+			{name:'harvey2',age:13},
+			{name:'harvey3',age:14}
+		]
+	},
+	methods:{
+		showOrNot:function(){
+			this.flag=!this.flag;
+		}
+	}
+
+})
+
+var app4=new Vue({
+	el:'#app4',
+	data:{
+		message:'default content'
+	}
+})
+app4.$watch('message',function(newValue,oldValue){
+		console.log('new value='+newValue,"   old value="+oldValue);
+	})
+
+
+Vue.component('my-component', {
+	props:['todo'],
+	template: '<div>A custom component!==&gt;{{todo}}</div>'
+})
+new Vue({
+	el:'#app5',
+	data:{
+		users:[
+			{name:'harvey1',age:12},
+			{name:'harvey2',age:13},
+			{name:'harvey3',age:14}
+		]
+	}
+
+})
